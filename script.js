6351a9a72e43c86d1b32da17973600a02c784a78
Phil Grana <104019839+phil-gran@users.noreply.github.com>
2023-04-27 08:15:33 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+function home(){
+  document.getElementById('home').style.display="block";
+  document.getElementById('salary').style.display="none";
+  document.getElementById('dialer').style.display="none";
+}
+function salary(){
+  document.getElementById('home').style.display="none";
+  document.getElementById('salary').style.display="block";
+  document.getElementById('dialer').style.display="none";
+}
+function dialer(){
+  document.getElementById('home').style.display="none";
+  document.getElementById('salary').style.display="none";
+  document.getElementById('dialer').style.display="block";
+}
+
+
+function btn1(){
+  document.getElementById('btn1').style.display="block";
+  document.getElementById('btn2').style.display="none";
+}
+function btn2(){
+  document.getElementById('btn1').style.display="none";
+  document.getElementById('btn2').style.display="block";
+}
+function btn3(){
+  document.getElementById('btn3').style.display="block";
+  document.getElementById('btn4').style.display="none";
+}
+function btn4(){
+  document.getElementById('btn3').style.display="none";
+  document.getElementById('btn4').style.display="block";
+}
+function btn5(){
+  document.getElementById('btn5').style.display="block";
+  document.getElementById('btn6').style.display="none";
+}
+function btn6(){
+  document.getElementById('btn5').style.display="none";
+  document.getElementById('btn6').style.display="block";
+}
+
+function clearBtn(){
+  document.getElementById('btn1').style.display="none";
+  document.getElementById('btn2').style.display="none";
+  document.getElementById('btn3').style.display="none";
+  document.getElementById('btn4').style.display="none";
+  document.getElementById('btn5').style.display="none";
+  document.getElementById('btn6').style.display="none";
+}
+
+
+
+
