da3c667ba2de3e93f6774d9922a874330bb046a9
Phil Grana <104019839+phil-gran@users.noreply.github.com>
2023-04-28 20:12:38 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,69 @@
 function home(){
   document.getElementById('home').style.display="block";
+  document.getElementById('script').style.display="none";
   document.getElementById('salary').style.display="none";
-  document.getElementById('dialer').style.display="none";
+  document.getElementById('tools').style.display="none";
+}
+function script(){
+  document.getElementById('home').style.display="none";
+  document.getElementById('script').style.display="block";
+  document.getElementById('salary').style.display="none";
+  document.getElementById('tools').style.display="none";
 }
 function salary(){
   document.getElementById('home').style.display="none";
+  document.getElementById('script').style.display="none";
   document.getElementById('salary').style.display="block";
-  document.getElementById('dialer').style.display="none";
+  document.getElementById('tools').style.display="none";
 }
-function dialer(){
+function tools(){
   document.getElementById('home').style.display="none";
+  document.getElementById('script').style.display="none";
   document.getElementById('salary').style.display="none";
-  document.getElementById('dialer').style.display="block";
+  document.getElementById('tools').style.display="block";
 }
+
+
+function whoIsShopVidi(){
+  document.getElementById('whoIsShopVidi').style.display="block";
+  document.getElementById('product').style.display="none";
+  document.getElementById('ourCustomers').style.display="none";
+  document.getElementById('yourRole').style.display="none";
+  document.getElementById('agentTools').style.display="none";
+}
+
+function product(){
+  document.getElementById('whoIsShopVidi').style.display="none";
+  document.getElementById('product').style.display="block";
+  document.getElementById('ourCustomers').style.display="none";
+  document.getElementById('yourRole').style.display="none";
+  document.getElementById('agentTools').style.display="none";
+}
+
+function ourCustomers(){
+  document.getElementById('whoIsShopVidi').style.display="none";
+  document.getElementById('product').style.display="none";
+  document.getElementById('ourCustomers').style.display="block";
+  document.getElementById('yourRole').style.display="none";
+  document.getElementById('agentTools').style.display="none";
+}
+
+function yourRole(){
+  document.getElementById('whoIsShopVidi').style.display="none";
+  document.getElementById('product').style.display="none";
+  document.getElementById('ourCustomers').style.display="none";
+  document.getElementById('yourRole').style.display="block";
+  document.getElementById('agentTools').style.display="none";
+}
+
+function agentTools(){
+  document.getElementById('whoIsShopVidi').style.display="none";
+  document.getElementById('product').style.display="none";
+  document.getElementById('ourCustomers').style.display="none";
+  document.getElementById('yourRole').style.display="none";
+  document.getElementById('agentTools').style.display="block";
+}
+
 
 
 function btn1(){
@@ -48,6 +99,26 @@ function clearBtn(){
   document.getElementById('btn5').style.display="none";
   document.getElementById('btn6').style.display="none";
 }
+
+function displayOff(){
+  document.getElementById('displayOff').style.display="block";
+  document.getElementById('howToRegister').style.display="none";
+  document.getElementById('howToOrder').style.display="none";
+}
+
+function howToRegister(){
+  document.getElementById('displayOff').style.display="none";
+  document.getElementById('howToRegister').style.display="block";
+  document.getElementById('howToOrder').style.display="none";
+}
+
+function howToOrder(){
+  document.getElementById('displayOff').style.display="none";
+  document.getElementById('howToRegister').style.display="none";
+  document.getElementById('howToOrder').style.display="block";
+}
+
+
 
 
 
